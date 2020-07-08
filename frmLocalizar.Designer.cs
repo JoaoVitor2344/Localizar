3b7f51bc39f67f4efd8a20ad941eddc417cf0ced
João Vitor <52049527+JoaoVitor2344@users.noreply.github.com>
2020-07-08 20:03:49 -0300

    Add files via upload

---
+++ frmLocalizar.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace CEP
+{
+    partial class frmLocalizar
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCEP = new MetroFramework.Controls.MetroLabel();
+            this.txtCEP = new MetroFramework.Controls.MetroTextBox();
+            this.btnConsultar = new MetroFramework.Controls.MetroButton();
+            this.wbMaps = new System.Windows.Forms.WebBrowser();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtEstado = new MetroFramework.Controls.MetroTextBox();
+            this.lblEstado = new MetroFramework.Controls.MetroLabel();
+            this.txtCidade = new MetroFramework.Controls.MetroTextBox();
+            this.lblCidade = new MetroFramework.Controls.MetroLabel();
+            this.txtBairro = new MetroFramework.Controls.MetroTextBox();
+            this.lblBairro = new MetroFramework.Controls.MetroLabel();
+            this.txtLogradouro = new MetroFramework.Controls.MetroTextBox();
+            this.lblLogradouro = new MetroFramework.Controls.MetroLabel();
+            this.btnLimpar = new MetroFramework.Controls.MetroButton();
+            this.btnSair = new MetroFramework.Controls.MetroButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblCEP
+            // 
+            this.lblCEP.AutoSize = true;
+            this.lblCEP.Location = new System.Drawing.Point(39, 92);
+            this.lblCEP.Name = "lblCEP";
+            this.lblCEP.Size = new System.Drawing.Size(33, 19);
+            this.lblCEP.TabIndex = 0;
+            this.lblCEP.Text = "CEP";
+            // 
+            // txtCEP
+            // 
+            // 
+            // 
+            // 
+            this.txtCEP.CustomButton.Image = null;
+            this.txtCEP.CustomButton.Location = new System.Drawing.Point(272, 1);
+            this.txtCEP.CustomButton.Name = "";
+            this.txtCEP.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtCEP.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtCEP.CustomButton.TabIndex = 1;
+            this.txtCEP.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtCEP.CustomButton.UseSelectable = true;
+            this.txtCEP.CustomButton.Visible = false;
+            this.txtCEP.Lines = new string[0];
+            this.txtCEP.Location = new System.Drawing.Point(138, 92);
+            this.txtCEP.MaxLength = 32767;
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.PasswordChar = '\0';
+            this.txtCEP.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtCEP.SelectedText = "";
+            this.txtCEP.SelectionLength = 0;
+            this.txtCEP.SelectionStart = 0;
+            this.txtCEP.ShortcutsEnabled = true;
+            this.txtCEP.Size = new System.Drawing.Size(294, 23);
+            this.txtCEP.TabIndex = 1;
+            this.txtCEP.UseSelectable = true;
+            this.txtCEP.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtCEP.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(548, 63);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(195, 51);
+            this.btnConsultar.TabIndex = 2;
+            this.btnConsultar.Text = "Consultar/Verificar";
+            this.btnConsultar.UseSelectable = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // wbMaps
+            // 
+            this.wbMaps.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.wbMaps.Location = new System.Drawing.Point(499, 145);
+            this.wbMaps.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbMaps.Name = "wbMaps";
+            this.wbMaps.Size = new System.Drawing.Size(515, 394);
+            this.wbMaps.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.txtEstado);
+            this.groupBox1.Controls.Add(this.lblEstado);
+            this.groupBox1.Controls.Add(this.txtCidade);
+            this.groupBox1.Controls.Add(this.lblCidade);
+            this.groupBox1.Controls.Add(this.txtBairro);
+            this.groupBox1.Controls.Add(this.lblBairro);
+            this.groupBox1.Controls.Add(this.txtLogradouro);
+            this.groupBox1.Controls.Add(this.lblLogradouro);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Location = new System.Drawing.Point(23, 136);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(470, 403);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informações";
+            // 
+            // txtEstado
+            // 
+            // 
+            // 
+            // 
+            this.txtEstado.CustomButton.Image = null;
+            this.txtEstado.CustomButton.Location = new System.Drawing.Point(272, 1);
+            this.txtEstado.CustomButton.Name = "";
+            this.txtEstado.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtEstado.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtEstado.CustomButton.TabIndex = 1;
+            this.txtEstado.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtEstado.CustomButton.UseSelectable = true;
+            this.txtEstado.CustomButton.Visible = false;
+            this.txtEstado.Lines = new string[0];
+            this.txtEstado.Location = new System.Drawing.Point(99, 346);
+            this.txtEstado.MaxLength = 32767;
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.PasswordChar = '\0';
+            this.txtEstado.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtEstado.SelectedText = "";
+            this.txtEstado.SelectionLength = 0;
+            this.txtEstado.SelectionStart = 0;
+            this.txtEstado.ShortcutsEnabled = true;
+            this.txtEstado.Size = new System.Drawing.Size(294, 23);
+            this.txtEstado.TabIndex = 12;
+            this.txtEstado.UseSelectable = true;
+            this.txtEstado.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtEstado.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(16, 346);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(48, 19);
+            this.lblEstado.TabIndex = 11;
+            this.lblEstado.Text = "Estado";
+            // 
+            // txtCidade
+            // 
+            // 
+            // 
+            // 
+            this.txtCidade.CustomButton.Image = null;
+            this.txtCidade.CustomButton.Location = new System.Drawing.Point(272, 1);
+            this.txtCidade.CustomButton.Name = "";
+            this.txtCidade.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtCidade.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtCidade.CustomButton.TabIndex = 1;
+            this.txtCidade.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtCidade.CustomButton.UseSelectable = true;
+            this.txtCidade.CustomButton.Visible = false;
+            this.txtCidade.Lines = new string[0];
+            this.txtCidade.Location = new System.Drawing.Point(99, 249);
+            this.txtCidade.MaxLength = 32767;
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.PasswordChar = '\0';
+            this.txtCidade.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtCidade.SelectedText = "";
+            this.txtCidade.SelectionLength = 0;
+            this.txtCidade.SelectionStart = 0;
+            this.txtCidade.ShortcutsEnabled = true;
+            this.txtCidade.Size = new System.Drawing.Size(294, 23);
+            this.txtCidade.TabIndex = 10;
+            this.txtCidade.UseSelectable = true;
+            this.txtCidade.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtCidade.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // lblCidade
+            // 
+            this.lblCidade.AutoSize = true;
+            this.lblCidade.Location = new System.Drawing.Point(16, 249);
+            this.lblCidade.Name = "lblCidade";
+            this.lblCidade.Size = new System.Drawing.Size(51, 19);
+            this.lblCidade.TabIndex = 9;
+            this.lblCidade.Text = "Cidade";
+            // 
+            // txtBairro
+            // 
+            // 
+            // 
+            // 
+            this.txtBairro.CustomButton.Image = null;
+            this.txtBairro.CustomButton.Location = new System.Drawing.Point(272, 1);
+            this.txtBairro.CustomButton.Name = "";
+            this.txtBairro.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtBairro.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtBairro.CustomButton.TabIndex = 1;
+            this.txtBairro.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtBairro.CustomButton.UseSelectable = true;
+            this.txtBairro.CustomButton.Visible = false;
+            this.txtBairro.Lines = new string[0];
+            this.txtBairro.Location = new System.Drawing.Point(99, 152);
+            this.txtBairro.MaxLength = 32767;
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.PasswordChar = '\0';
+            this.txtBairro.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtBairro.SelectedText = "";
+            this.txtBairro.SelectionLength = 0;
+            this.txtBairro.SelectionStart = 0;
+            this.txtBairro.ShortcutsEnabled = true;
+            this.txtBairro.Size = new System.Drawing.Size(294, 23);
+            this.txtBairro.TabIndex = 8;
+            this.txtBairro.UseSelectable = true;
+            this.txtBairro.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtBairro.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // lblBairro
+            // 
+            this.lblBairro.AutoSize = true;
+            this.lblBairro.Location = new System.Drawing.Point(16, 152);
+            this.lblBairro.Name = "lblBairro";
+            this.lblBairro.Size = new System.Drawing.Size(45, 19);
+            this.lblBairro.TabIndex = 7;
+            this.lblBairro.Text = "Bairro";
+            // 
+            // txtLogradouro
+            // 
+            // 
+            // 
+            // 
+            this.txtLogradouro.CustomButton.Image = null;
+            this.txtLogradouro.CustomButton.Location = new System.Drawing.Point(272, 1);
+            this.txtLogradouro.CustomButton.Name = "";
+            this.txtLogradouro.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtLogradouro.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtLogradouro.CustomButton.TabIndex = 1;
+            this.txtLogradouro.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtLogradouro.CustomButton.UseSelectable = true;
+            this.txtLogradouro.CustomButton.Visible = false;
+            this.txtLogradouro.Lines = new string[0];
+            this.txtLogradouro.Location = new System.Drawing.Point(99, 55);
+            this.txtLogradouro.MaxLength = 32767;
+            this.txtLogradouro.Name = "txtLogradouro";
+            this.txtLogradouro.PasswordChar = '\0';
+            this.txtLogradouro.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtLogradouro.SelectedText = "";
+            this.txtLogradouro.SelectionLength = 0;
+            this.txtLogradouro.SelectionStart = 0;
+            this.txtLogradouro.ShortcutsEnabled = true;
+            this.txtLogradouro.Size = new System.Drawing.Size(294, 23);
+            this.txtLogradouro.TabIndex = 6;
+            this.txtLogradouro.UseSelectable = true;
+            this.txtLogradouro.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtLogradouro.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // lblLogradouro
+            // 
+            this.lblLogradouro.AutoSize = true;
+            this.lblLogradouro.Location = new System.Drawing.Point(16, 55);
+            this.lblLogradouro.Name = "lblLogradouro";
+            this.lblLogradouro.Size = new System.Drawing.Size(79, 19);
+            this.lblLogradouro.TabIndex = 5;
+            this.lblLogradouro.Text = "Logradouro";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(939, 49);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseSelectable = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(939, 92);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 6;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseSelectable = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // frmLocalizar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 562);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.wbMaps);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.lblCEP);
+            this.MaximizeBox = false;
+            this.Name = "frmLocalizar";
+            this.Style = MetroFramework.MetroColorStyle.Red;
+            this.Text = "Consultar CEP";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel lblCEP;
+        private MetroFramework.Controls.MetroTextBox txtCEP;
+        private MetroFramework.Controls.MetroButton btnConsultar;
+        private System.Windows.Forms.WebBrowser wbMaps;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private MetroFramework.Controls.MetroTextBox txtEstado;
+        private MetroFramework.Controls.MetroLabel lblEstado;
+        private MetroFramework.Controls.MetroTextBox txtCidade;
+        private MetroFramework.Controls.MetroLabel lblCidade;
+        private MetroFramework.Controls.MetroTextBox txtBairro;
+        private MetroFramework.Controls.MetroLabel lblBairro;
+        private MetroFramework.Controls.MetroTextBox txtLogradouro;
+        private MetroFramework.Controls.MetroLabel lblLogradouro;
+        private MetroFramework.Controls.MetroButton btnLimpar;
+        private MetroFramework.Controls.MetroButton btnSair;
+    }
+}
+
---
+++ frmLocalizar.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+namespace CEP
+{
+    public partial class frmLocalizar : MetroFramework.Forms.MetroForm
+    {
+        public frmLocalizar()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(txtCEP.Text))
+            {
+                using (var ws = new WSCorreios.AtendeClienteClient())
+                {
+                    try
+                    {
+                        var endereco = ws.consultaCEP(txtCEP.Text.Trim());
+                        txtLogradouro.Text = endereco.end;
+                        txtBairro.Text = endereco.bairro;
+                        txtCidade.Text = endereco.cidade;
+                        txtEstado.Text = endereco.uf;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+            string street = string.Empty;
+            string city = string.Empty;
+            string state = string.Empty;
+            string zip = string.Empty;
+            string district = string.Empty;
+
+            StringBuilder queryAddress = new StringBuilder();
+            queryAddress.Append("http://maps.google.com/maps?q=");
+
+            if (txtLogradouro.Text != string.Empty)
+            {
+                street = txtLogradouro.Text.Replace(' ', '+');
+                queryAddress.Append(street + ',' + '+');
+            }
+
+            if (txtBairro.Text != string.Empty)
+            {
+                district = txtBairro.Text.Replace(' ', '+');
+                queryAddress.Append(district + ',' + '+');
+            }
+
+            if (txtCidade.Text != string.Empty)
+            {
+                city = txtCidade.Text.Replace(' ', '+');
+                queryAddress.Append(city + ',' + '+');
+            }
+
+            if (txtEstado.Text != string.Empty)
+            {
+                state = txtEstado.Text.Replace(' ', '+');
+                queryAddress.Append(state + ',' + '+');
+            }
+
+            if (txtCEP.Text != string.Empty)
+            {
+                zip = txtCEP.Text.Replace(' ', '+');
+                queryAddress.Append(zip + ',' + '+');
+            }
+
+            wbMaps.Navigate(queryAddress.ToString());
+            wbMaps.ScriptErrorsSuppressed = true;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtCEP.Text = string.Empty;
+            txtLogradouro.Text = string.Empty;
+            txtBairro.Text = string.Empty;
+            txtCidade.Text = string.Empty;
+            txtEstado.Text = string.Empty;
+            wbMaps.DocumentText = null;
+        }
+    }
+}
